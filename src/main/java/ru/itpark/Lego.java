899d8bd79290962282c0a805182dc6a05d8464f8
student <student@localhost>
2019-10-02 19:41:51 +0300

    initial commit

---
+++ src/main/java/ru/itpark/Lego.java
@@ -0,0 +1,10 @@
+package ru.itpark;
+
+public class Lego {
+    private long id;
+    private String name;
+    private String imageUrl;
+    private int price;
+    private int discountPrice;
+    private boolean available;
+}
